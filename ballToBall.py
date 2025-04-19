eb36180073416a971444297e2a43adf4dba31742
Rushi1110 <50615477+Rushi1110@users.noreply.github.com>
2025-04-19 14:14:11 +0530

    Add files via upload

---
+++ ballToBall.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 19 14:12:21 2025
+
+@author: Rushi
+"""
+
+rom fastapi import FastAPI, Request, HTTPException
+from pydantic import BaseModel
+import firebase_admin
+from firebase_admin import credentials, auth, firestore
+
+red = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+app = FastAPI()
+
+db = firestore.client()
+
+
+@app.get("/balls")
+def get_all_balls():
+    docs = db.collection("ball_ID").stream()
+
+    all_balls = []
+    for doc in docs:
+        ball_data = doc.to_dict()
+        all_balls.append(ball_data)
+
+    return {"balls": all_balls}
